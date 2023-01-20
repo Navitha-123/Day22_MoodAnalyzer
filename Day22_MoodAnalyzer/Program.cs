e095890eb6b7d8688dad0131d5beaa63452cbf9c
navitha <navithareddy112@gmail.com>
2023-01-20 21:05:06 +0530

    [navitha] UC1MoodAnalyzer

---
+++ Day22_MoodAnalyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Day22_MoodAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            Random random = new Random();
+            int mood = random.Next(2);
+
+            if (mood == 1)
+            {
+                message = "Happy";
+            }
+            else
+            {
+                message = "Sad";
+
+            }
+            Console.WriteLine(UC1MoodAnalyzer.analyzeMood(message));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day22_MoodAnalyzer/UC1MoodAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_MoodAnalyzer
+{
+    public class UC1MoodAnalyzer
+    {
+        public static string analyzeMood(string message)
+        {
+            return message;
+        }
+    }
+}
